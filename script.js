470ad6aea0996bb653d96b47f7c9066309094465
Romer Chia <romerchia@hotmail.com>
2021-06-30 16:03:52 -0400

    fixed directory error in script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 /*Function to include Top navigation to a page*/
 $(function(){
-    $("#includeTopNav").load("/topNav.html"); 
+    $("#includeTopNav").load("topNav.html"); 
 });
 
 /*Function to include Footer Bar to a page*/
 $(function(){
-    $("#includeFooter").load("/footer.html"); 
+    $("#includeFooter").load("footer.html"); 
 });
 
 $(document).ready(function() {
